3723f104df9b382ad9ea2fe8d7b6f2c85bb5dba1
elraffa <federicoruffa@gmail.com>
2020-10-27 20:59:19 -0300

    Add 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,8 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404: No encontrado</h1>
+    <h3 style={{ textAlign: center }}>La página que está buscando no existe. <a href='/'>Volver</a></h3>
   </Layout>
 )
 
