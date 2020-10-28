4de04bd2eeece62ce2393512fb928ebc86130a0e
elraffa <federicoruffa@gmail.com>
2020-10-28 10:40:55 -0300

    Fix 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>404: No encontrado</h1>
-    <h3 style={{ textAlign: center }}>La página que está buscando no existe. <a href='/'>Volver</a></h3>
+    <h3 style={{ textAlign: 'center' }}>La página que está buscando no existe. <a href='/'>Volver</a></h3>
   </Layout>
 )
 
