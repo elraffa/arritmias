f528250036c3a96d161a3ac84e3f6c44143dc644
elraffa <federicoruffa@gmail.com>
2020-11-12 14:16:51 -0300

    Change index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,12 @@ import Layout from "../components/layout"
 // import Image from "../components/image"
 import SEO from "../components/seo"
 import { Container, Col, Row } from 'react-bootstrap'
-import MainSlider from '../components/slider'
 import Launch from '../components/launch'
 import Welcome from '../components/welcome'
 import Invitados from "../components/invitados"
 import Programa from '../components/programa'
 import Sponsors from '../components/sponsors'
 import Comite from '../components/comite'
-import Footer from '../components/footer'
 import metaImage from '../images/banner-web-2020.jpg'
 
 
