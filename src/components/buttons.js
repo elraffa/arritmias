fb1ea944403c00ebc5a98c972cce24ebf75df271
elraffa <federicoruffa@gmail.com>
2020-11-20 11:06:51 -0300

    add button certificate

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -5,8 +5,8 @@ const Buttons = () => (
 
     <Row style={{ alignItems: 'center', textAlign: 'center', marginBottom: 20, justifyContent: 'space-between' }}>
         <Col>
-            <Button className='newButton'><a href="#">Descargue su certificado</a></Button>
-            <Button className='newButton'><a href="#">Acceda al contenido On Demand</a></Button>
+            <Button className='newButton'><a href="https://intercloudy.contilatam.com/certificados/index.php?idCertificado=264" target='_blank'>Descargue su certificado</a></Button>
+            {/* <Button className='newButton'><a href="#">Acceda al contenido On Demand</a></Button> */}
         </Col>
     </Row>
 
