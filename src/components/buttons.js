9520d2250a2a0531308f9404773c3b09ff0dffd8
elraffa <federicoruffa@gmail.com>
2020-12-21 14:14:24 -0300

    Add button

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -3,9 +3,13 @@ import { Row, Col, Button } from 'react-bootstrap'
 
 const Buttons = () => (
 
-    <Row style={{ alignItems: 'center', textAlign: 'center', marginBottom: 20, justifyContent: 'space-between' }}>
+    <Row style={{ alignItems: 'center', textAlign: 'center', marginBottom: 20}}>
         <Col>
             <Button className='newButton'><a href="https://intercloudy.contilatam.com/certificados/index.php?idCertificado=264" target='_blank'>Descargue su certificado</a></Button>
+            {/* <Button className='newButton'><a href="#">Acceda al contenido On Demand</a></Button> */}
+        </Col>
+        <Col>
+            <Button className='newButton'><a href="https://intercloudy.contilatam.com/programa-cientifico/27/encuentro-nacional-de-arritmias-edicion-virtual" target='_blank'>Ver el Contenido On Demand</a></Button>
             {/* <Button className='newButton'><a href="#">Acceda al contenido On Demand</a></Button> */}
         </Col>
     </Row>
